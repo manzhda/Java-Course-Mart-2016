7d9ce3fbbf66356c4c0436fac664f9e084f24bdf
Dmytro Manzhosov <manzhda@gmail.com>
2016-05-30 20:04:14 +0300

    Added space.

--- src/db/DBMain.java
+++ src/db/DBMain.java
@@ -11,6 +11,9 @@ public class DBMain {
     public static void main(String[] args) throws SQLException {
 
         Connection dbConnection = DBUtilsSample.getDBConnection();
+
+
+        
         Statement statement = dbConnection.createStatement();
 
 
